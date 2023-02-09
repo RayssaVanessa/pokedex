81823f649a8cb4e448e5dc231064f06095bd3f10
RayssaVanessa <rayssav.ss@gmail.com>
2023-02-09 00:03:42 -0300

    adiciona navegacao e parte da conf da toolbar

---
+++ app/src/main/java/com/example/curso_android_marvel/presentation/PokemonFragment.kt
@@ -0,0 +1,20 @@
+package com.example.curso_android_marvel.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.curso_android_marvel.R
+
+class PokemonFragment : Fragment() {
+
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_pokemon, container, false)
+        }
+
+    }
--- app/src/main/java/com/example/curso_android_marvel/presentation/PokerActivity.kt
+++ app/src/main/java/com/example/curso_android_marvel/presentation/PokerActivity.kt
@@ -1,28 +1,33 @@
-package com.example.curso_android_marvel
+package com.example.curso_android_marvel.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.curso_android_marvel.R
 import com.example.curso_android_marvel.databinding.PokerActivityBinding
 
 class PokerActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-    private lateinit var biding: PokerActivityBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        biding = PokerActivityBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        val biding = PokerActivityBinding.inflate(layoutInflater)
         setContentView(R.layout.poker_activity)
 
         //recuperando meu navHost(fragmento que vai receber outro dentro pra conseguir fazer a navegação)
         //dentro dele que vai ter meus destinos
         val navHostFragment = supportFragmentManager
-            .findFragmentById(androidx.navigation.fragment.R.id.nav_host_fragment_container) as NavHostFragment
-
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
         navController = navHostFragment.navController
+
+        //vai controlr a navegacao
+        biding.bottomNavMain.setupWithNavController(navController)
+
         appBarConfiguration = AppBarConfiguration(
         setOf()
         )
