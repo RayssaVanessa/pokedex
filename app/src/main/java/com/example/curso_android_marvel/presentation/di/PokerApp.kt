37cfd8a83bc81e71ce9c1f04a4e05853ed8bc4d3
RayssaVanessa <rayssav.ss@gmail.com>
2023-02-10 00:24:31 -0300

    mapeamento dos objetos responses do pokemon

---
+++ app/src/main/java/com/example/curso_android_marvel/presentation/di/PokerApp.kt
@@ -0,0 +1,9 @@
+package com.example.curso_android_marvel.presentation.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokerApp : Application() {
+
+}
